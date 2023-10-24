5aebb2f03ae9abceb2b77ee25ca04aa1c5479bb2
papum_debian <danieledugo1@gmail.com>
2023-10-24 15:42:20 +0200

    remove package:homepage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import React, {Component} from 'react';
-import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
+import React, { Component } from 'react';
+import {
+  BrowserRouter as Router, Switch, Route, Redirect
+} from 'react-router-dom';
 import { ThemeProvider } from '@material-ui/core';
-import { Provider as ReduxProvider } from "react-redux";
+import { Provider as ReduxProvider } from 'react-redux';
 
 import DashboardLayout from 'src/views/DashboardLayout/DashboardLayout';
 import LoginView from 'src/views/auth/LoginView';
@@ -9,33 +11,32 @@ import RegisterView from 'src/views/auth/RegisterView';
 import LoggerView from 'src/views/logger/Logger';
 import NotFoundView from 'src/views/errors/NotFoundView';
 import GitlabPage from 'src/views/gitlab/GitlabPage';
-import TaigaPage from 'src/views/taiga/TaigaPage'
-import SonarPage from 'src/views/sonar/SonarPage'
+import TaigaPage from 'src/views/taiga/TaigaPage';
+import SonarPage from 'src/views/sonar/SonarPage';
 
 import theme from 'src/theme/theme';
 import store from 'src/redux/store/store';
-import {config} from 'src/services/config';
+import { config } from 'src/services/config';
 
-class App extends Component{
-
+class App extends Component {
   render() {
     return (
       <ThemeProvider theme={theme}>
         <ReduxProvider store={store}>
           <Router basename={process.env.PUBLIC_URL}>
-            <DashboardLayout >
+            <DashboardLayout>
               <Switch>
                 <Route exact path="/">
                   <Redirect to="/logger" />
                 </Route>
                 <Route exact path="/login" component={LoginView} />
-                <Route exact path="/register" component={RegisterView}/>
-                <Route exact path='/logger' component={LoggerView}/>
-                <Route exact path='/gitlab' component={GitlabPage}/>
-                <Route exact path='/taiga' component={TaigaPage}/>
-                <Route exact path='/sonar' component={SonarPage}/>
-                <Route exact path='/jenkins' component={() => window.location = `${config.URL}/jenkins`}/>
-                <Route path="/*" component={NotFoundView}/>
+                <Route exact path="/register" component={RegisterView} />
+                <Route exact path="/logger" component={LoggerView} />
+                <Route exact path="/gitlab" component={GitlabPage} />
+                <Route exact path="/taiga" component={TaigaPage} />
+                <Route exact path="/sonar" component={SonarPage} />
+                <Route exact path="/jenkins" component={() => window.location = `${config.URL}/jenkins`} />
+                <Route path="/*" component={NotFoundView} />
               </Switch>
             </DashboardLayout>
           </Router>
